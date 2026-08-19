3a7bc482bc85a57b0690f65fea6135da79a2f572
TheRealShadowCoder <125616204+TheRealShadowCoder@users.noreply.github.com>
2026-08-19 03:00:17 +0200

    Install Google Cloud browser routing before application startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './google-cloud-routing';
 import App from './App';
 import { ExplainabilityLayer } from './components/ExplainabilityLayer';
 import { QualityFrameworkDock } from './components/QualityFrameworkDock';
